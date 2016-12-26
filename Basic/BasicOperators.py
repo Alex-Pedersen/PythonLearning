e74fd8892bce2cb2db63d24d2db59f275daf80e5
Alex-Pedersen <1038281@ucn.dk>
2016-12-26 02:44:13 +0100

    Arithmetic Operands

---
+++ Basic/BasicOperators.py
@@ -0,0 +1,25 @@
+# Arithmetic Operators
+a = 10
+b = 20
+
+# Addition Adds values on either side of the operator.
+print("Addition ", (a + b))
+
+# Subtraction Subtracts right hand operand from left hand operand.
+print("Subtraction ", (a - b))
+
+# Multiplication Multiplies values on either side of the operator Divison Divides left hand operand by right hand
+# operand
+print("Multiplication ", (a * b))
+
+# Modulus Divides left hand operand by right hand operand and returns remainder Exponent Performs exponential
+#  (power) calculation on operators
+print("Modulus", (a % b))
+
+# Exponent Performs exponential (power) calculation on operators
+print("Exponent", a ** b)
+
+# Floor Division - The division of operands where the result is the
+# quotient in which the digits after the decimal point are removed. But if one of the operands is negative,
+# the result is floored, i.e., rounded away from zero (towards negative infinity)
+print("Floor division", (a // b))
